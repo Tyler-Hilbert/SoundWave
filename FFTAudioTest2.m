1ec25bc71aea2dd4e097fa8c5822b0c789f2af6c
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2018-01-08 12:55:54 -0300

    Moving window of FFT with overlap

--- FFTAudioTest2.m
+++ FFTAudioTest2.m
@@ -1,5 +1,3 @@
-% Taking a second of an audio file and breaking it into n many chunks and
-% figuring out what frequencies make up each of those chunks
 clear all;
 
 % Read Audio
@@ -16,30 +14,28 @@ end
 full = full(crop:end);
 
 % Perform fft and get frequencies
-chunks = 16;         % How many chunks to break wave into
-for i = 1:chunks
-    if i > 40
-        break;
-    end
-    beginningChunk = (i-1)*fs/chunks+1;
-    endChunk = i*fs/chunks;
+windowSize = fs/16;
+for i = windowSize/2+1 : fs/32 : fs*5
+    beginningChunk = i-windowSize/2;
+    endChunk = i+windowSize/2;
     x = full(beginningChunk:endChunk);
     y = fft(x);
     n = length(x);     % number of samples in chunk
-    amp = abs(y)/n;    % amplitude of the DFT
+    amp = abs(y).^2/n;    % amplitude of the DFT
     amp = amp(1:end/2);
     f = (0:n-1)*(fs/n);     % frequency range
     f = f(1:end/2);
 
-    pause(1);
+    pause(.0005);
     figure(1);
     plot(f,amp)
-    axis([0 20000 0 .02]);
+    axis([0 10000 0 3]);
     xlabel('Frequency')
     ylabel('amplitude')
+    max(amp)
 end
 
-test = full((1-1)*fs/chunks+1:1*fs/chunks);
-hplayer = audioplayer(test, fs);
-play(hplayer);
+% test = full((1-1)*fs/chunks+1:1*fs/chunks);
+% hplayer = audioplayer(test, fs);
+% play(hplayer);
 
