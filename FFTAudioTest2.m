c6a5bd8e5ac78bc8257373fc8441dbd349cfe0ba
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2018-01-09 02:55:22 -0300

    Rolling window in real time is somewhat functional

--- FFTAudioTest2.m
+++ FFTAudioTest2.m
@@ -4,7 +4,7 @@ clear all;
 fs = 44100;         % sample frequency (Hz)
 full = audioread('song.wav');
 
-% Remove leading 0's
+% Remove leading 0's and select range
 for i = 1:fs
     if full(i) ~= 0
         crop = i;
@@ -13,26 +13,36 @@ for i = 1:fs
 end
 full = full(crop:end);
 
-% Perform fft and get frequencies
-windowSize = fs/16;
-for i = windowSize/2+1 : fs/32 : fs*5
-    beginningChunk = i-windowSize/2;
-    endChunk = i+windowSize/2;
+startTime = 0;
+endTime = 50;
+
+% Play song
+tic
+player = audioplayer(full(fs*startTime+1:fs*endTime), fs);
+player.play();
+initialTime = toc;
+
+% Perform fft and get frequencies (hopefully in realish time with audio)
+windowSize = fs/8;
+for i = windowSize/2+1+fs*startTime : fs/16 : fs*endTime
+    beginningChunk = round(i-windowSize/2);
+    endChunk = round(i+windowSize/2);
+    
     x = full(beginningChunk:endChunk);
     y = fft(x);
-    n = length(x);     % number of samples in chunk
-    amp = abs(y).^2/n;    % amplitude of the DFT
-    amp = amp(1:end/2);
+    n = length(x);          % number of samples in chunk
+    power = abs(y).^2/n;    % power of the DFT
+    power = power(1:end/2);
     f = (0:n-1)*(fs/n);     % frequency range
     f = f(1:end/2);
-
-    pause(.0005);
+    while initialTime+i/fs > toc
+        pause(.0001);
+    end
     figure(1);
-    plot(f,amp)
-    axis([0 10000 0 3]);
-    xlabel('Frequency')
-    ylabel('amplitude')
-    max(amp)
+    plot(f,power);
+    axis([0 10000 0 5]);
+    xlabel('Frequency');
+    ylabel('amplitude');
 end
 
 % test = full((1-1)*fs/chunks+1:1*fs/chunks);
