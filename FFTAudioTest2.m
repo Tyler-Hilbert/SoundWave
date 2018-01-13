fbcbc4a1b6c7d9f107568e2a6e78d53b5471fd62
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2018-01-13 04:26:50 -0300

    Comments and stuff

--- FFTAudioTest2.m
+++ FFTAudioTest2.m
@@ -1,4 +1,6 @@
+% Prototype to graph moving window of FFT through an audio file in real time
 clear all;
+warning('off','MATLAB:colon:nonIntegerIndex'); % Suppress integer operand errors because it's ok to round for the window size
 
 % Read Audio
 fs = 44100;         % sample frequency (Hz)
@@ -14,7 +16,7 @@ end
 full = full(crop:end);
 
 startTime = 0;
-endTime = 50;
+endTime = length(full)/fs;
 
 % Play song
 tic
@@ -25,9 +27,11 @@ initialTime = toc;
 % Perform fft and get frequencies (hopefully in realish time with audio)
 windowSize = fs/8;
 for i = windowSize/2+1+fs*startTime : fs/16 : fs*endTime
+    % Get chunk to process
     beginningChunk = round(i-windowSize/2);
     endChunk = round(i+windowSize/2);
     
+    % Calculate power for each frequency
     x = full(beginningChunk:endChunk);
     y = fft(x);
     n = length(x);          % number of samples in chunk
@@ -35,17 +39,19 @@ for i = windowSize/2+1+fs*startTime : fs/16 : fs*endTime
     power = power(1:end/2);
     f = (0:n-1)*(fs/n);     % frequency range
     f = f(1:end/2);
+    
+    % Emphasis filter
+%     power = power(1:end).*f(1:end); % Note this probably isn't a good emphasis filter
+    
+    % Wait for audio to catch back up
     while initialTime+i/fs > toc
         pause(.0001);
     end
+    % Plot
     figure(1);
     plot(f,power);
     axis([0 10000 0 5]);
     xlabel('Frequency');
-    ylabel('amplitude');
+    ylabel('Power');
 end
-
-% test = full((1-1)*fs/chunks+1:1*fs/chunks);
-% hplayer = audioplayer(test, fs);
-% play(hplayer);
 
